21baafe342195cea992b68b3b04065a0bcfa2591
harrison314 <harrison314@azet.sk>
2020-12-10 17:38:43 +0100

    Add Global attributes.

---
+++ src/src/Harrison314.EntityFrameworkCore.Encryption/Global.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Harrison314.EntityFrameworkCore.Encryption.Contrib")]
